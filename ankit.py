6264abd16686af8a777ab59668233d7e23b52b0f
loop-hack <timetraveller1732@gmail.com>
2024-12-13 19:41:15 +0530

    variables

--- ankit.py
+++ ankit.py
@@ -1,6 +1,45 @@
-def add_numbers(a, b):
+# defining a function
+
+"""def add_numbers(a, b):
       sum = a + b
       return sum
 
 result= add_numbers(5,3)
 print(result)
+"""
+
+#defining main to help us write any function in any order
+#giving function some parameter also defining its default value
+
+"""def main():
+      name= input("What's your name? ")
+      hello()
+      hello(name)
+      
+def hello(to="World"):
+      print("Hello,",to)
+
+main()
+"""
+
+#using return while defining a function
+
+def main():
+      x = int(input("What's x? "))
+      print("x square is",square(x))
+
+def square(n):
+      return n*n
+#or we can do 
+
+""" return n**2 """
+
+""" ** means we want power and next number says how much"""
+
+#or we can do 
+
+""" return pow(n,2) """
+      
+""" pow(n,2) it is python library which is used for squaring a number"""
+
+main()
