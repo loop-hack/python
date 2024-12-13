815b73c7c1e74dcb313ff180f4bf63acd9af0053
loop-hack <timetraveller1732@gmail.com>
2024-12-13 18:44:20 +0530

    removing comment

--- function.py
+++ function.py
@@ -1,31 +1,29 @@
 #function creation
 
-"""def add_numbers(a, b):
+def add_numbers(a, b):
       sum = a + b
       return sum
 
 result= add_numbers(5,3)
 print(result)
-"""
+
 #printing name and assigning variable the input given by users:
 
-"""name= input("What is your name? ")
+name= input("What is your name? ")
 print(f"Hello, {name}")
-"""
+
 
 #using strip() and capitalize() function
 
-"""name= input("What is your name? ")
+name= input("What is your name? ")
 name = name.strip()
 name= name.capitalize()
 print(f"Hello, {name}")
-"""
 
 #using strip() and title() for capitalizing name and making it redable
 
-"""name= input("What is your name? ").strip().title()
+name= input("What is your name? ").strip().title()
 print(f"Hello, {name}")
-"""
 
 #using split()
 
