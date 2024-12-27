e67f7b1c046e81fb4ba0234b1ec71a6f7abafc84
loop-hack <timetraveller1732@gmail.com>
2024-12-27 20:46:49 +0530

    simplification of code

--- Conditions/parity.py
+++ Conditions/parity.py
@@ -36,3 +36,22 @@ def is_even(n) :
     return True if n%2 == 0 else False
 
 main()
+
+
+#more simplified version
+
+def main() :
+    if is_even(x) :
+        print("Even")
+    else :
+        print("False")
+
+def is_even(n) :
+    return n%2 == 0
+
+    """ we are asking here to return the 
+    value of n%2 == 0 that means we know for sure
+    it will give either true or false ... 
+    if n even it will return true and execute if statment otherwise
+    return false that will result in execution of else statement."""
+main()
