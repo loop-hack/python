cdc06bee85629b7ef45b2d9029a706a1ffe580e8
loop-hack <timetraveller1732@gmail.com>
2024-12-28 21:44:49 +0530

    dict

--- loop/hogwarts.py
+++ loop/hogwarts.py
@@ -15,3 +15,41 @@ for i in range(len(students)):
 students = ["Hermione", "Harry","Ron"]
 for i in range(len(students)):
     print(i + 1, students[i])
+
+
+#dictionary {}
+
+students = { "Hermione":"Griffindor",
+            "Harry":"Griffindor",
+            "Ron":"Griffindor",
+            "Draco":"Slytherin" }
+
+print(students["Hermione"])
+print(students["Harry"])
+print(students["Ron"])
+print(students["Draco"])
+
+#improvement in code 
+
+students = { "Hermione":"Griffindor",
+            "Harry":"Griffindor",
+            "Ron":"Griffindor",
+            "Draco":"Slytherin" }
+
+for student in students:
+    print(student) 
+
+
+"""only key will print from here we can say 
+in students elements are only key not value both together
+"""
+
+# to print both house and name of students we should do
+
+students = { "Hermione":"Griffindor",
+            "Harry":"Griffindor",
+            "Ron":"Griffindor",
+            "Draco":"Slytherin" }
+
+for student in students:
+    print(student,students[student],sep=", ")
