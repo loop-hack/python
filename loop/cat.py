0cbd7a27d617109e31df0ae81f632c96366f43dd
loop-hack <timetraveller1732@gmail.com>
2024-12-28 19:59:17 +0530

    meow

---
+++ loop/cat.py
@@ -0,0 +1,26 @@
+# while lopp
+
+i = 3
+while i != 0:
+    print("Meow")
+    i = i - 1
+
+
+#for loop
+
+for i in [0,1,2]:
+    print("meow")
+
+#we are not using i in code thats why replce it by _
+
+for _ in range(3):
+    print("meow")
+
+
+#for print meow 3 times we can also do
+
+print("meow"*3)  #output meowmeowmeow
+
+print("meow\n"*3) #output one extra newline bcz by default print has newline
+
+print("meow\n"*3,end="")
