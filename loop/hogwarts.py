e78fbcc6792a3c5422fb8cb33a7c18aa386e0257
loop-hack <timetraveller1732@gmail.com>
2024-12-28 21:12:43 +0530

    list,len.i

--- loop/hogwarts.py
+++ loop/hogwarts.py
@@ -3,3 +3,15 @@
 students = ["Hermione", "Harry","Ron"]
 for student in students:
     print(student)
+
+#using len and i for for loop iteration
+
+students = ["Hermione", "Harry","Ron"]
+for i in range(len(students)):
+    print(students[i])
+
+#we can do more with this like i can add rank like whose 1,2,3
+
+students = ["Hermione", "Harry","Ron"]
+for i in range(len(students)):
+    print(i + 1, students[i])
