47e128dd38894a69778862f3d7c0d9999433b841
loop-hack <timetraveller1732@gmail.com>
2024-12-28 13:13:18 +0530

    Using match case

---
+++ house.py
@@ -0,0 +1,24 @@
+name = input("What's your name? ")
+
+if name == "Harry" or name == "Hermione" or name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else :
+    print("Who?")
+
+
+#Using match
+
+name = input("What's your name? ")
+match name :
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?") #here case _ means all other cases other than previous
