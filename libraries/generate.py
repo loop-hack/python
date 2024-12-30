b4f9bf2a2a7b40fe0a659674052abea7bb0e4dce
loop-hack <timetraveller1732@gmail.com>
2024-12-30 21:33:55 +0530

    using statistics library

--- libraries/generate.py
+++ libraries/generate.py
@@ -10,3 +10,22 @@ print(coin)
 from random import choice
 coin = choice(["heads","tails"])
 print(coin)
+
+
+#how get a random no between a range example [1,10]
+'''using random.randint(1,10)'''
+
+import random
+
+number = random.randint(1,10)
+print(number)
+
+
+#How to shuffle elements given in our list (eg. list of cards)
+
+import random
+
+cards = ["King", "Queen", "Jack"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
