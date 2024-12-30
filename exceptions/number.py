078cbc9dc9ba109dc58977002a310f1847c55479
loop-hack <timetraveller1732@gmail.com>
2024-12-30 19:09:16 +0530

    handling errors

--- exceptions/number.py
+++ exceptions/number.py
@@ -1,1 +1,40 @@
 x = int(input("What's x? "))
+print(f"x is {x}")
+
+
+""" Output: What's x? cat
+  ValueError: invalid literal for int() with base 10: 'cat'
+  this problem arises because we expected an integral value 
+  from user but got cat ... to resolve this problem we can do -->
+"""
+
+
+#try except use:
+
+try:
+    x = int(input("What's x? "))
+    print("x is {x}")
+except ValueError:
+    print("x is not an integer")
+
+#more sodfisticated way to do this is
+
+try :
+    x = int(input("What's x? "))
+except ValueError:
+    print("x is not an integer")
+else:
+    print(f"x is {x}")
+
+
+#let's create a loop where users will be asked if wrong input
+
+while True:
+    try:
+        x = int(input("What's x? "))
+    except ValueError:
+        print("x is not an integer")
+    else :
+        break
+
+print(f"x is {x}")
