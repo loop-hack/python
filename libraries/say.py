62cded92a66c5814082532d5592c0ab70414bc03
loop-hack <timetraveller1732@gmail.com>
2024-12-31 21:15:54 +0530

    trex in cowsay

--- libraries/say.py
+++ libraries/say.py
@@ -1,5 +1,15 @@
+#cow printing in terminal
+
 import cowsay
 import sys
 
 if len(sys.argv)==2:
     cowsay.cow("hello, " + sys.argv[1])
+
+#trex
+
+import cowsay
+import sys
+
+if len(sys.argv)==2:
+    cowsay.trex("hello, " + sys.argv[1])
