b9def30db46954bb8bdec1e442633379fe845c7e
loop-hack <timetraveller1732@gmail.com>
2025-01-01 18:18:20 +0530

    using pytest for automate my testing

--- unit_tests/test_calculator.py
+++ unit_tests/test_calculator.py
@@ -1,4 +1,4 @@
-from calculator1 import square
+'''from calculator1 import square
 
 def main():
     test_square()
@@ -13,6 +13,36 @@ def test_square():
         assert square(3) == 9
     except AssertionError:
         print("3 square was not 9")
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 square was not 4")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("-3 square was not 9")
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("0 square was not 0")
 
 if __name__ == "__main__":
     main()
+'''
+
+"""as we can see above code fro testing has so many lines
+that's why we gonna use pytest library that will do try except
+printing for me and bcz of that our code will have less lines"""
+
+#using pytest library which makes testing easier
+
+
+from calculator1 import square
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
+
