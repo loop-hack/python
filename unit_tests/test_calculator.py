f7cb34a21fd393b7ae81098b140e9cd1593f3d9e
loop-hack <timetraveller1732@gmail.com>
2025-01-01 18:42:31 +0530

    pytest

--- unit_tests/test_calculator.py
+++ unit_tests/test_calculator.py
@@ -37,7 +37,7 @@ printing for me and bcz of that our code will have less lines"""
 #using pytest library which makes testing easier
 
 
-from calculator1 import square
+'''from calculator1 import square
 
 def test_square():
     assert square(2) == 4
@@ -45,4 +45,25 @@ def test_square():
     assert square(-2) == 4
     assert square(-3) == 9
     assert square(0) == 0
+'''
 
+""" in above code when something will be wrong 
+lets say -2 is not 4 then then in terminal pytest only shows 
+upto first error it does'nt test remaining and not provide list of every assertion that went wrong"""
+
+#crreating seperate test so that pytest check cases each indivisualy
+
+
+from calculator1 import square
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+def test_zero():
+    assert square(0) == 0
