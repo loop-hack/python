b715438c28f00a3b998ddcb34de4f6b83c419ef6
loop-hack <timetraveller1732@gmail.com>
2025-01-02 18:32:42 +0530

    using 'a' for append

--- File_I_O/names.py
+++ File_I_O/names.py
@@ -9,7 +9,15 @@ for name in sorted(names):
 
 #open() is used to store data given in terminal
 
+"""name = input("What's your name? ")
+file = open("name1.txt","w")
+file.write(name)
+file.close()
+"""
+
+#"a" for appending that will store ever input given
+
 name = input("What's your name? ")
-file = open("name.txt","w")
+file = open("name2.txt","a")
 file.write(name)
 file.close()
