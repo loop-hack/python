365fb445a5048a1ec5b539df309711e9a3345928
loop-hack <timetraveller1732@gmail.com>
2025-01-05 21:07:58 +0530

    finishing .csv file type

--- File_I_O/students.py
+++ File_I_O/students.py
@@ -1,4 +1,4 @@
-'''with open("students.csv") as file:
+with open("students.csv") as file:
     for line in file:
         row = line.rstrip().split(",")
         print(f"{row[0]} is in {row[1]}")
@@ -22,12 +22,11 @@ with open("students.csv") as file:
 for student in sorted(students):
     print(student)
 
-'''
 
 """previous code is not upto that mark because we are sorting whole sentece 
 we want to short by name , we just got lucky bcs name was first in sentance"""
 
-"""students = []
+students = []
 
 with open("students.csv") as file:
     for line in file:
@@ -41,11 +40,11 @@ def get_name(student):
     return student["name"]
 
 for student in sorted(students,key=get_name):
-    print(f"{student['name']} is in {student['house']}")"""
+    print(f"{student['name']} is in {student['house']}")
 
 '''we can shorten this up by shortening the way we have defined dictionary student{}'''
 
-"""students = []
+students = []
 
 with open("students.csv") as file:
     for line in file:
@@ -61,17 +60,17 @@ def get_house(student):
 
 for student in sorted(students,key=get_name):
     print(f"{student['name']} is in {student['house']}")
-"""
+
 
 """for sorting by name of house from Z to A"""
 
-"""for student in sorted(students,key=get_house,reverse=True):
+for student in sorted(students,key=get_house,reverse=True):
     print(f"{student['name']} is in {student['house']}")
-"""
+
 '''we are creating a function or not using it twice for this we can short these code
 by using lmbda that creates instance function just like we used _ for unsuded variable in for loop'''
 
-"""students = []
+students = []
 
 with open("students.csv") as file:
     for line in file:
@@ -81,12 +80,12 @@ with open("students.csv") as file:
 
 for student in sorted(students,key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
-"""
+
 
 '''let's say we have more than one comma in line .. in our csv file we have data something like
 1)Ankit,Ambedkarnagar , Budhauli,Sheikhpura [3comma](2)Ron,The Burrow [1 comma] then how we gonna print name and adress'''
 
-"""import csv 
+import csv 
 
 students = []
 
@@ -98,12 +97,12 @@ with open("students2.csv") as file:
         students.append({"name":name,"home":home})
 
 for student in sorted(students,key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")"""
+    print(f"{student['name']} is from {student['home']}")
 
 '''we have some changes in our .csv file in first row i wrote name,house to make it useful for 
 dictreader that is creating entire file into dictionary'''
 
-"""import csv 
+import csv 
 
 students = []
 
@@ -113,7 +112,7 @@ with open("students3.csv") as file:
         students.append({"name":row['name'],"home":row['home']})
 
 for student in sorted(students,key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")"""
+    print(f"{student['name']} is from {student['home']}")
 
 #to write .csv file data from code we can do :
 
