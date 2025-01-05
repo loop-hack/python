ac45b36b5fcc1c546dcd9f6ef487f94562ed002a
loop-hack <timetraveller1732@gmail.com>
2025-01-05 15:08:15 +0530

    using lamda function

--- File_I_O/students.py
+++ File_I_O/students.py
@@ -1,4 +1,4 @@
-with open("students.csv") as file:
+'''with open("students.csv") as file:
     for line in file:
         row = line.rstrip().split(",")
         print(f"{row[0]} is in {row[1]}")
@@ -10,3 +10,73 @@ with open("students.csv") as file:
     for line in file:
         name,house = line.rstrip().split(",")
         print(f"{name} is in {house}")
+
+#for shorting 
+
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name,house = line.rstrip().split(",")
+        students.append(f"{name} is in {house}")
+for student in sorted(students):
+    print(student)
+
+'''
+
+"""previous code is not upto that mark because we are sorting whole sentece 
+we want to short by name , we just got lucky bcs name was first in sentance"""
+
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name,house = line.rstrip().split(",")
+        student = {}
+        student["name"] = name
+        student["house"] = house
+        students.append(student)
+
+def get_name(student):
+    return student["name"]
+
+for student in sorted(students,key=get_name):
+    print(f"{student['name']} is in {student['house']}")
+
+'''we can shorten this up by shortening the way we have defined dictionary student{}'''
+
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name,house = line.rstrip().split(",")
+        student = {"name": name, "house":house}
+        students.append(student)
+
+def get_name(student):
+    return student["name"]
+
+def get_house(student):
+    return student["house"]
+
+for student in sorted(students,key=get_name):
+    print(f"{student['name']} is in {student['house']}")
+
+"""for sorting by name of house from Z to A"""
+
+for student in sorted(students,key=get_house,reverse=True):
+    print(f"{student['name']} is in {student['house']}")
+
+'''we are creating a function or not using it twice for this we can short these code
+by using lmbda that creates instance function just like we used _ for unsuded variable in for loop'''
+
+students = []
+
+with open("students.csv") as file:
+    for line in file:
+        name,house = line.rstrip().split(",")
+        student = { "name": name,"house":house}
+        students.append(student)
+
+for student in sorted(students,key=lambda student: student["name"]):
+    print(f"{student['name']} is in {student['house']}")
