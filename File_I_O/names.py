c280891042159734074fb76fb7cf4e9b1cf6f15e
loop-hack <timetraveller1732@gmail.com>
2025-01-05 12:03:24 +0530

    shorting

--- File_I_O/names.py
+++ File_I_O/names.py
@@ -1,4 +1,4 @@
-names = []
+'''names = []
 
 for _ in range(3):
     names.append(input("What's your name? "))
@@ -21,15 +21,17 @@ name = input("What's your name? ")
 file = open("name2.txt","a")
 file.write(name)
 file.close()
+'''
 
 
 """output : AnkitDavidRam bcz 'a' 
 doesn't add new line after each input"""
 
-name = input("What's your name? ")
+"""name = input("What's your name? ")
 file = open("name3.txt","a")
 file.write(f"{name}\n")
 file.close()
+
 
 
 # to read "r"
@@ -38,4 +40,24 @@ with open("name3.txt","r") as file:
     lines = file.readlines()
 
 for line in lines:
+    print("hello,",line)
+
+'''this will print an exta line bcz this is coming from list of file'''
+
+with open("name3.txt","r") as file:
+    lines = file.readlines()
+
+for line in lines:
     print("hello,",line.rstrip())
+"""
+
+# now we want to short our read names then print it
+
+names = []
+
+with open("name3.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"hello, {name}")
