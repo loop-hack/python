90e481d21359b75766935401f202c288ffa960c7
loop-hack <timetraveller1732@gmail.com>
2025-01-05 21:00:27 +0530

    writing .csv file

--- File_I_O/students.py
+++ File_I_O/students.py
@@ -27,7 +27,7 @@ for student in sorted(students):
 """previous code is not upto that mark because we are sorting whole sentece 
 we want to short by name , we just got lucky bcs name was first in sentance"""
 
-students = []
+"""students = []
 
 with open("students.csv") as file:
     for line in file:
@@ -41,11 +41,11 @@ def get_name(student):
     return student["name"]
 
 for student in sorted(students,key=get_name):
-    print(f"{student['name']} is in {student['house']}")
+    print(f"{student['name']} is in {student['house']}")"""
 
 '''we can shorten this up by shortening the way we have defined dictionary student{}'''
 
-students = []
+"""students = []
 
 with open("students.csv") as file:
     for line in file:
@@ -61,16 +61,17 @@ def get_house(student):
 
 for student in sorted(students,key=get_name):
     print(f"{student['name']} is in {student['house']}")
+"""
 
 """for sorting by name of house from Z to A"""
 
-for student in sorted(students,key=get_house,reverse=True):
+"""for student in sorted(students,key=get_house,reverse=True):
     print(f"{student['name']} is in {student['house']}")
-
+"""
 '''we are creating a function or not using it twice for this we can short these code
 by using lmbda that creates instance function just like we used _ for unsuded variable in for loop'''
 
-students = []
+"""students = []
 
 with open("students.csv") as file:
     for line in file:
@@ -80,12 +81,12 @@ with open("students.csv") as file:
 
 for student in sorted(students,key=lambda student: student["name"]):
     print(f"{student['name']} is in {student['house']}")
-
+"""
 
 '''let's say we have more than one comma in line .. in our csv file we have data something like
 1)Ankit,Ambedkarnagar , Budhauli,Sheikhpura [3comma](2)Ron,The Burrow [1 comma] then how we gonna print name and adress'''
 
-import csv 
+"""import csv 
 
 students = []
 
@@ -97,19 +98,41 @@ with open("students2.csv") as file:
         students.append({"name":name,"home":home})
 
 for student in sorted(students,key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")
+    print(f"{student['name']} is from {student['home']}")"""
 
 '''we have some changes in our .csv file in first row i wrote name,house to make it useful for 
 dictreader that is creating entire file into dictionary'''
 
-import csv 
+"""import csv 
 
 students = []
 
-with open("students2.csv") as file:
+with open("students3.csv") as file:
     reader = csv.DictReader(file)
     for row in reader:
         students.append({"name":row['name'],"home":row['home']})
 
 for student in sorted(students,key=lambda student: student["name"]):
-    print(f"{student['name']} is from {student['home']}")
+    print(f"{student['name']} is from {student['home']}")"""
+
+#to write .csv file data from code we can do :
+
+import csv 
+
+name = input("what's your name? ")
+home = input("what's your house? ")
+
+with open("students4.csv","a",newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow([name,home])
+
+# below one is creating dictionary it's good for data that has key and value
+
+import csv
+
+name = input("what's your name? ")
+home = input("where's your home? ")
+
+with open("students5.csv","a",newline='') as file:
+    writer = csv.DictWriter(file,fieldnames=["name","home"])
+    writer.writerow({"name":name,"home":home})
